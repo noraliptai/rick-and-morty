64259a77d89116f2ae3ce105b1483e750bf92a2b
Your Name <you@example.com>
2023-11-08 11:16:44 +0100

    first version

---
+++ script.js
@@ -0,0 +1,25 @@
+const rootElement = document.getElementById("root")
+
+const fetchUrl = (url) => fetch(url).then(res => res.json())
+
+const characterComponent = (characterData) => `
+    <div class="char">
+        <img src=${characterData.image}>
+        <h2>${characterData.name}</h2>
+        <h3>appears in: ${characterData.episode.length} episodes</h3>
+    </div>
+`
+
+const init = () => {
+    fetchUrl("https://rickandmortyapi.com/api/character").then(data => {
+
+        const info = data.info
+        const characters = data.results
+
+        characters.forEach(character => {
+            rootElement.insertAdjacentHTML("beforeend", characterComponent(character))
+        });
+    })
+}
+
+init()
